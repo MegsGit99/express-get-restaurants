20eb59f170de88e74a2acdb52d565f55a9cc7725
MegsGit99 <129975225+MegsGit99@users.noreply.github.com>
2023-05-09 15:44:36 +0100

    1 file modified

--- src/app.js
+++ src/app.js
@@ -4,7 +4,22 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
+app.use(express.static('public'))
 
+app.get('/restaurants', async (req, res) => {
+    const restaurants = await Restaurant.findAll();
+})
+
+app.get('restaurants/:id', async (req, res) => {
+    const restaurant = await Restaurant.findByPk(id);
+    if (restaurant) {
+        
+    }
+})
+
+app.listen(3000, () => {
+    console.log(`Server is up and running at http://localhost:3000`);
+});
 
 
 
